f8e6167633b2fa953c32c62806d95e04c26c48d6
caojiameng <41064570+caojiameng@users.noreply.github.com>
2018-08-07 17:29:46 +0800

    Delete FileService.java

--- src/main/java/com/hand/api/service/FileService.java
+++
@@ -1,9 +0,0 @@
-package com.hand.api.service;
-
-import com.hand.domain.entity.Film;
-import com.hand.infra.mapper.FilmMapper;
-
-public interface FilmService {
-    public int insertFilm(Film film);
-    public Film selectFilm(Integer id);
-}
