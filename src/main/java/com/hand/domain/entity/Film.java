b99ae5e0c37acc4cfaaf0494f7f0b1b6f0e7ab3c
caojiameng <41064570+caojiameng@users.noreply.github.com>
2018-08-07 17:00:24 +0800

    Create Film.java

---
+++ src/main/java/com/hand/domain/entity/Film.java
@@ -0,0 +1,53 @@
+package com.hand.domain.entity;
+
+public class Film {
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Integer languageId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Integer languageId) {
+        this.languageId = languageId;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", languageId=" + languageId +
+                '}';
+    }
+}
